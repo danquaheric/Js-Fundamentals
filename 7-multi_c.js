32d21a0a3c2af63f4e39e5ff37258113939269f0
Eric Danquah <danquah@aircellsolutions.com>
2025-11-06 19:57:05 +0000

    Add scripts that prints x times 'C is fun' using while loop

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,9 +1,14 @@
-const args = process.argv.slice(2);
+// File: 7-multi_c.js
 
+const args = process.argv.slice(2);
 const x = parseInt(args[0]);
 
-if(isNaN(x)) {
-    console.log("Missing number of occurrences");
+if (isNaN(x)) {
+  console.log("Missing number of occurrences");
 } else {
-    for(let i = 0; i < x; i++) console.log("C is fun");
+  let i = 0;
+  while (i < x) {
+    console.log("C is fun");
+    i++;
+  }
 }
